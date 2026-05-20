d9edc1018988e14585eaa95968c2659d1be086e7
DarkHole1 <dark.hole1@yandex.ru>
2026-05-20 21:56:22 +0300

    Fixed runner

--- start.sh
+++ start.sh
@@ -2,20 +2,20 @@
 
     # -Djdk.graal.PrintGraph=Network \
     # -Dgraal.Dump=Truffle:1 \
+    # -Dpolyglot.engine.TraceCompilationDetails=true \
+    # -Dpolyglot.engine.AllowExperimentalOptions=true \
+    # -Dpolyglot.engine.BackgroundCompilation=false \
+    # -Dpolyglot.engine.CompilationFailureAction=Diagnose \
+    # -Dpolyglot.engine.TraceCompilationAST=true \
+    # -Dpolyglot.compiler.TraceInlining=true \
+    # -Dpolyglot.compiler.TracePerformanceWarnings=all \
+    # -Dpolyglot.engine.TraceTransferToInterpreter=true \
+    # -Dpolyglot.engine.TraceAssumptions=true \
+    # -Dpolyglot.engine.TraceSplitting=true \
+    # -XX:+UnlockDiagnosticVMOptions \
+    # -XX:+DebugNonSafepoints \
+    # -XX:+TraceDeoptimization \
 java \
     -Xss128M -Xms1g -Xmx8g \
-    -Dpolyglot.engine.TraceCompilationDetails=true \
-    -Dpolyglot.engine.AllowExperimentalOptions=true \
     -Dpolyglot.engine.TraceCompilation=true \
-    -Dpolyglot.engine.BackgroundCompilation=false \
-    -Dpolyglot.engine.CompilationFailureAction=Diagnose \
-    -Dpolyglot.engine.TraceCompilationAST=true \
-    -Dpolyglot.compiler.TraceInlining=true \
-    -Dpolyglot.compiler.TracePerformanceWarnings=all \
-    -Dpolyglot.engine.TraceTransferToInterpreter=true \
-    -Dpolyglot.engine.TraceAssumptions=true \
-    -Dpolyglot.engine.TraceSplitting=true \
-    -XX:+UnlockDiagnosticVMOptions \
-    -XX:+DebugNonSafepoints \
-    -XX:+TraceDeoptimization \
     -cp "target/lama-truffle-1.0.0.jar:target/dependency/*" com.lama.truffle.App "$1" >graal.log 2>&1
